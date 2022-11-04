3e6898111e4d076cdf9032b306a6a7d6b9c7f569
Lucas Dainez <lucdainez@gmail.com>
2022-11-04 16:31:37 -0300

    feat(index.js): criar endpoint
    foi criado um ENDPOINT para enviar em caso de sucesso todo o array com objetos e em caso de falha um status 404 com um array vazio

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getAllTalkers } = require('./talker');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.use(express.json());
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await getAllTalkers();
+  if (talkers) {
+    return res.status(200).json(talkers);
+  }
+  return res.status(404).json([]);
 });
 
 app.listen(PORT, () => {
